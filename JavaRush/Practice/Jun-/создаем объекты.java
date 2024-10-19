e45681aca1599da8934f1e456c2c236181576baa
glebprogbox <118668832+glebprogbox@users.noreply.github.com>
2024-10-20 00:54:28 +0300

    Create создаем объекты.java

---
+++ JavaRush/Practice/Jun-/создаем объекты.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String st1 = new String();
+        String st2 = new String();
+        String st3 = new String();
+        String st4 = new String();
+        String st5 = new String();
+        String st6 = new String();
+        String st7 = new String();
+        String st8 = new String();
+        String st9 = new String();
+        String st10 = new String();
+        int[] in1 = new int[1];
+        int[] in2 = new int[2];
+        int[] in3 = new int[3];
+        int[] in4 = new int[4];
+        int[] in5 = new int[5];
+        Scanner sc1 = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+    }
+}
+
